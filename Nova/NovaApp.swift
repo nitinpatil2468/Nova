bad426a110d8abd216a5db418ff87b97e10e9fc4
nitinpatil2468 <nitin@getpi.in>
2024-12-02 10:57:18 +0530

    Initial Commit

---
+++ Nova/NovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovaApp.swift
+//  Nova
+//
+//  Created by Abcom on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
